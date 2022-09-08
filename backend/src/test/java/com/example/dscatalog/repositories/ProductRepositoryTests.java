1dd331c671465e6647cc8672aa858fc08e9b2a3e
Gustavo Fadel <Gustavo-959694@hotmail.com>
2022-09-08 00:43:22 -0300

    adicionado uma factory nos testes

--- backend/src/test/java/com/example/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/example/dscatalog/repositories/ProductRepositoryTests.java
@@ -1,2 +1,54 @@
-package com.example.dscatalog.repositories;public class ProductRepositoryTests {
+package com.example.dscatalog.repositories;
+
+import com.example.dscatalog.entities.Product;
+import com.example.dscatalog.tests.Factory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+    @Autowired
+    private ProductRepository productRepository;
+
+    private long existId;
+    private long nonExistId;
+
+
+    @BeforeEach
+     void setUp(){
+        existId = 1L;
+        nonExistId = 1005L;
+    }
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists(){
+        productRepository.deleteById(existId);
+        Optional<Product> product = productRepository.findById(existId);
+
+        Assertions.assertTrue(product.isEmpty());
+    }
+    @Test
+    public void deleteShouldsThrowsEmptyResultDataAccessExceptionWhereIdDoesNotExist(){
+
+        Assertions.assertThrows(EmptyResultDataAccessException.class,()->{
+            productRepository.deleteById(nonExistId);
+        });
+    }
+    @Test
+    public void saveShouldsPersistWhereIdIsNullAndAutoIncrement(){
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        product = productRepository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+
+    }
 }
--- backend/src/test/java/com/example/dscatalog/tests/Factory.java
+++ backend/src/test/java/com/example/dscatalog/tests/Factory.java
@@ -1,4 +1,19 @@
 package com.example.dscatalog.tests;
 
+import com.example.dscatalog.dto.ProductDTO;
+import com.example.dscatalog.entities.Category;
+import com.example.dscatalog.entities.Product;
+
+import java.time.Instant;
+
 public class Factory {
+    public static Product createProduct(){
+        Product product = new Product(1L, "Phone", "Phone is nice", 800.00,"htttps://img.com", Instant.parse("2020-07-13T20:50:07.12345Z"));
+        product.getCategories().add(new Category(2L, "Electronics"));
+        return product;
+    }
+    public static ProductDTO createProductDto(){
+        Product product = createProduct();
+        return new ProductDTO(product,product.getCategories());
+    }
 }
