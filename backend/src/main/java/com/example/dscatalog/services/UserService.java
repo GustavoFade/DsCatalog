f818876c8fdf1952bb00fb358ab5cfe476846879
Gustavo Fadel <Gustavo-959694@hotmail.com>
2022-09-15 01:05:41 -0300

    implementado OAuth2 authorization server

--- backend/src/main/java/com/example/dscatalog/services/UserService.java
+++ backend/src/main/java/com/example/dscatalog/services/UserService.java
@@ -17,6 +17,9 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,7 +28,7 @@ import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
     @Autowired
@@ -92,5 +95,14 @@ public class UserService {
             Role role = roleRepository.getOne(roleDTO.getId());
             entity.getRoles().add(role);
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null){
+            throw new UsernameNotFoundException("Email not found");
+        }
+        return user;
     }
 }
